7f2f5542f59db9ba1533f70db177dd694cfd87ac
marc <marc.foletto@gmail.com>
2017-11-07 16:30:45 +0100

    add remoteApp and lirc-node dependancie

--- modules/users/commons/roles.js
+++ modules/users/commons/roles.js
@@ -4,7 +4,7 @@
 const ADMIN_ROLE = {id: 'ADMIN_ROLE', name: 'admin'};
 const USER_ROLE = {id: 'USER_ROLE', name: 'user'};
 const INVIT_ROLE = {id: 'INVIT_ROLE', name: 'invit'};
-const MARC_ROLE = {id: 'MARC_ROLE', name: 'marc'};
+const REMOTEAMP_ROLE = {id: 'REMOTEAMP_ROLE', name: 'remoteAmp'};
 
 const DEFAULT_AUTH_ROLE = USER_ROLE;
 
@@ -12,7 +12,7 @@ module.exports = {
     ADMIN_ROLE: ADMIN_ROLE,
     USER_ROLE: USER_ROLE,
     INVIT_ROLE: INVIT_ROLE,
-    MARC_ROLE: MARC_ROLE,
+    REMOTEAMP_ROLE: REMOTEAMP_ROLE,
     DEFAULT_AUTH_ROLE: DEFAULT_AUTH_ROLE,
-    ALL_ROLE: [ADMIN_ROLE, USER_ROLE, INVIT_ROLE, MARC_ROLE],
+    ALL_ROLE: [ADMIN_ROLE, USER_ROLE, INVIT_ROLE, REMOTEAMP_ROLE],
 };
