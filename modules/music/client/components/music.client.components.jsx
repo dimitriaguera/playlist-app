5f9a0441b7a3de2f6d7220c23c8dddd24cb3ab12
Dimitri <dimitri.aguera@gmail.com>
2017-11-28 22:01:48 +0100

    album and artist indexation

---
+++ modules/music/client/components/music.client.components.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { get } from 'core/client/services/core.api.services'
+import { playOnPlaylist } from 'music/client/redux/actions'
+import SearchMusicBar from './searchMusicBar.client.components'
+import Tracks from './tracks.client.components'
+import { Divider, Label, Button, Segment } from 'semantic-ui-react'
+
+
+class Music extends Component {
+
+    constructor( props ) {
+
+        super(props);
+
+        this.state = {
+            nodes: [],
+        }
+    }
+
+    componentWillMount() {
+
+    }
+
+    // Unmount and delete socket.
+    componentWillUnmount() {
+
+    }
+
+
+    render(){
+
+        const { nodes } = this.state;
+
+        return (
+            <div>
+                <h1>Music</h1>
+                <SearchMusicBar handlerResult={result => this.setState({nodes: result})} />
+
+                {
+                    nodes.map((item, i) => {
+                        return (
+                            <div key={i}>
+                                {item.name}
+                            </div>
+                        );
+                    })
+                }
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        user: state.authenticationStore._user,
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        readFile: ( item ) => dispatch(
+            playOnPlaylist( item )
+        ),
+    }
+};
+
+const MusicContainer = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Music);
+
+export default MusicContainer
