5647076c7e60b7f6118c1f22aabfd24eddb8b7e8
marc <marc.foletto@gmail.com>
2017-12-14 18:13:00 +0100

    Bug correction on trimFct for reading tag

--- modules/music/server/services/metaTag/metaTag.server.services.js
+++ modules/music/server/services/metaTag/metaTag.server.services.js
@@ -37,11 +37,13 @@ try {
  * @returns {*}
  */
 function trimObj(obj) {
-
-  if (!Array.isArray(obj) && typeof obj !== 'object') return obj;
+  if (!obj) {
+    console.log('sa pue');
+  }
+  if ( !Array.isArray(obj) || typeof obj !== 'object' || !obj ) return obj;
 
   return Object.keys(obj).reduce(function(acc, key) {
-    acc[key.trim()] = typeof obj[key] === 'string'? obj[key].trim() : trimObj(obj[key]);
+    acc[key.trim()] = typeof obj[key] === 'string' ? obj[key].trim() : trimObj(obj[key]);
     return acc;
   }, Array.isArray(obj)? []:{});
 
