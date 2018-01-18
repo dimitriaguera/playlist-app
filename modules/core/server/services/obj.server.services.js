fa00f3d657638ec83268c74fc92aa34850e65c9e
dimitriaguera <dimitri.aguera@gmail.com>
2018-01-18 12:56:33 +0100

    add methof to obj.server.services

--- modules/core/server/services/obj.server.services.js
+++ modules/core/server/services/obj.server.services.js
@@ -1,7 +1,7 @@
 /**
  * Created by Marc Foletto on 18/12/2017.
  */
-
+const _ = require('lodash');
 
 /**
  * Trim obj and key of an obj in deep
@@ -31,17 +31,70 @@ exports.splitTab = function(files, nbToSplit) {
   return tabOnWork;
 };
 
+// /**
+//  * Push an element on a Array only if
+//  * doesn't exist
+//  *
+//  * @param data
+//  * @param array
+//  */
+// exports.pushUniq = function(data , array){
+//
+//   if (array.findIndex(elmt => elmt === data) === -1 ){
+//     array.push(data);
+//   }
+//
+// };
+
+
 /**
  * Push an element on a Array only if
  * doesn't exist
  *
- * @param newData
- * @param Array
+ * @param data
+ * @param array
+ * @param callee
  */
-exports.pushUniq = function(newData , Array){
+exports.pushUniq = function(data , array, callee){
 
-  if (Array.findIndex(elmt => elmt === newData) === -1 ){
-    Array.push(newData);
-  }
+  const call = callee ? callee : elmt => elmt === data;
 
+    if (array.findIndex(call) === -1 ){
+        array.push(data);
+    }
+
+};
+
+/**
+ * Compare old adn new array and return added or deleted values.
+ *
+ * @param theOlds
+ * @param theNews
+ * @returns {{deleted: *, added: *}}
+ */
+exports.compareArray = function( theOlds, theNews ){
+
+    const deleted = _.difference(theOlds, theNews);
+    const added = _.difference(theNews, theOlds);
+
+    return {
+      deleted: deleted,
+        added: added,
+    };
+};
+
+/**
+ * Merge 2 arrays if needed. If no, return false.
+ * @param arr1
+ * @param arr2
+ * @returns {*}
+ */
+exports.testMergeArray = function(arr1, arr2) {
+    // Concat arr1 and 2, remove clones
+    const newArray = _.uniq(arr1.concat(arr2));
+    // If more entry, return concat array.
+    if( newArray.length !== arr1.length ) {
+        return newArray;
+    }
+    return false;
 };
