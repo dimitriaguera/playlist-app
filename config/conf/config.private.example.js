c1f1b2a2d87ec5ea51763f6d091937c8d1bb7a23
dimitri aguera <dimitri.aguera@gmail.com>
2018-02-24 21:50:21 +0100

    add bcryptSaltRounds option in conf

--- config/conf/config.private.example.js
+++ config/conf/config.private.example.js
@@ -53,6 +53,7 @@ module.exports = {
 
   security: {
     jwtSecret: 'SECRET', // You need to change (Add number, special char, mix upper case lower case)
+    bcryptSaltRounds: 12,
     secureFile: /(^[^.].*)/,
     illegalUsernames: [
       'administrator', 'password', 'user',
