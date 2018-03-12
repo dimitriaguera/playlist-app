7050e1aae7527da0f178a0358e90e8b28afc4e2d
dimitriaguera <dimitri.aguera@gmail.com>
2018-03-12 17:57:50 +0100

    add date service on core

---
+++ modules/core/client/services/core.date.services.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Dimitri on 19/10/2017.
+ */
+
+const monthNames = [
+  "January", "February", "March",
+  "April", "May", "June", "July",
+  "August", "September", "October",
+  "November", "December"
+];
+
+// Return formated date from string formated input date.
+exports.getFormatedDate = function(string) {
+  const date = new Date(string);
+  const day = date.getDate();
+  const monthIndex = date.getMonth();
+  const year = date.getFullYear();
+
+  return day + ' ' + monthNames[monthIndex] + ' ' + year;
+};
+
